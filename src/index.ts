981711f989bce4819b12468f4d086d478edfff77
Mohammed Samrose <samrose.mohammed@gmail.com>
2024-10-13 01:33:20 +0545

    custom docker image

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-const learnDocker: string | boolean = true;
-if (learnDocker) {
-  console.log("Hello, Docker!");
-}
+import express from "express";
+const app = express();
+const PORT: number | string = process.env.PORT || 3000;
+
+app.get("/", (req: any, res: any) => {
+  return res.json({ message: "Hey, I am nodejs in container" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
