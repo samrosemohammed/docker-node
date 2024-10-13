6df894adcd9b066c6f528a2042c7e9c1a5170d61
Mohammed Samrose <samrose.mohammed@gmail.com>
2024-10-13 17:06:48 +0545

    add some environment configure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
 const PORT: number | string = process.env.PORT || 3000;
-
+app.use(cors());
 app.get("/", (req: any, res: any) => {
-  return res.json({ message: "Hey, I am nodejs in container" });
+  return res.json([
+    {
+      id: 1,
+      name: "Roshan Thapa Magar",
+    },
+    {
+      id: 2,
+      name: "Suman Paudel",
+    },
+    {
+      id: 3,
+      name: "Ishaan Pudasaini",
+    },
+  ]);
 });
 
 app.listen(PORT, () => {
